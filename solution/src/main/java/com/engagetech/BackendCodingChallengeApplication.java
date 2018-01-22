e9b160cca0cd97657a281d0970f089eb33a9c828
Effie Makri <effiemakri@EffieMak-iMac.local>
2018-01-22 13:15:05 +0200

    first commit

---
+++ solution/src/main/java/com/engagetech/BackendCodingChallengeApplication.java
@@ -0,0 +1,12 @@
+package com.engagetech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCodingChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCodingChallengeApplication.class, args);
+	}
+}
