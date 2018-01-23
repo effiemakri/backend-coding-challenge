f72ad597ce66028881dbe4cf2d75b86a76fd4e2e
Effie Makri <effiemakri@EffieMak-iMac.local>
2018-01-23 11:01:52 +0200

    updated expenses-controller.js to display newly created expense

--- src/js/apps/codingtest/expenses/expenses-controller.js
+++ src/js/apps/codingtest/expenses/expenses-controller.js
@@ -17,6 +17,7 @@ app.controller("ctrlExpenses", ["$rootScope", "$scope", "config", "restalchemy",
 	$rootScope.selectTabSection("expenses", 0);
 
 	var restExpenses = $restalchemy.init({ root: $config.apiroot }).at("expenses");
+	$scope.expenses = [];
 
 	$scope.dateOptions = {
 		changeMonth: true,
@@ -29,14 +30,19 @@ app.controller("ctrlExpenses", ["$rootScope", "$scope", "config", "restalchemy",
 		restExpenses.get().then(function(expenses) {
 			$scope.expenses = expenses;
 		});
+		$scope.clearExpense();
 	}
 
 	$scope.saveExpense = function() {
 		if ($scope.expensesform.$valid) {
+			// calculate the VAT:
+			$scope.newExpense.vat = $scope.newExpense.amount * (1 - 1/1.2);
 			// Post the expense via REST
 			restExpenses.post($scope.newExpense).then(function() {
+				// ensure that the newly-created expense is displayed on the page immediately
+				$scope.expenses.push($scope.newExpense);
 				// Reload new expenses list
-				loadExpenses();
+				// loadExpenses();
 			});
 		}
 	};
